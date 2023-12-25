9948fb2773fdc5b4668176acfc39135cbc8e952f
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2023-12-25 10:11:46 +0530

    Refactor exception handling and add network functionality

--- Practices/Main.java
+++ Practices/Main.java
@@ -1,22 +1,15 @@
-class MyException extends Exception {
-    MyException(String s) {
-        super(s);
-    }
-}
+import java.io.*;
+import java.net.*;
 
 public class Main {
-    static void m1() throws MyException {
-        throw new MyException(null);
-    }
-
     public static void main(String[] args) {
         try {
-            m1();
-        } catch (MyException e) {
-            System.out.println(e + e.getMessage());
-        } finally {
-            System.out.println("Finally block");
+            InetAddress ip = InetAddress.getByName("www.javatpoint.com");
+
+            System.out.println("Host Name: " + ip);
+            System.out.println("IP Address: " + ip.getHostAddress());
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        System.out.println("Outside block - main()");
     }
 }
